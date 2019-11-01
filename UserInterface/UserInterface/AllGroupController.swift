77f8a07990fee649a5afb51ea6a0334f5ec6d3d2
Kandalova Elena <elena@kandalova.ru>
2019-11-02 00:42:39 +0300

    Исправлены констрейнты на форме ввода пароля - центрирование по супервью задано. Сделаны 2 таблицы, настроены отображения ячеек.

---
+++ UserInterface/UserInterface/AllGroupController.swift
@@ -0,0 +1,30 @@
+//
+//  AllGroupController.swift
+//  UserInterface
+//
+//  Created by Elena Kandalova on 29/10/2019.
+//  Copyright © 2019 Elena Kandalova. All rights reserved.
+//
+
+import UIKit
+
+class AllGroupController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
